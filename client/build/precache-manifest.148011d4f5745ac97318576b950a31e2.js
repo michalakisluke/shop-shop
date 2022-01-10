b1d297562312cc7e159ccc6897ea589d688b9887
Luke Michalakis <michalakisluke@gmail.com>
2022-01-10 14:31:35 -0500

    Deploying

---
+++ client/build/precache-manifest.148011d4f5745ac97318576b950a31e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffdb41adc137e9724f3802bfc026e563",
+    "url": "/index.html"
+  },
+  {
+    "revision": "679655687e3e1d72c57e",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "183ecf38862b767fe164",
+    "url": "/static/js/2.28f58591.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.28f58591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679655687e3e1d72c57e",
+    "url": "/static/js/main.470dbc1d.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
